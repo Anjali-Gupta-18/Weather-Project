b3daec6353efc1b09f8980e52460fe29bd5f5596
Anjali-Gupta-18 <anjalik3.gupta18@gmail.com>
2024-05-28 15:36:54 +0100

    added few lines

--- weather_project/weather_app/tests.py
+++ weather_project/weather_app/tests.py
@@ -1,6 +1,3 @@
-from django.test import TestCase
-
-# Create your tests here.
 from django.test import TestCase, Client
 from django.urls import reverse
 from .models import City, Weather
@@ -16,6 +13,26 @@ class WeatherAppTestCase(TestCase):
             temperature=20.0,
             description="clear sky"
         )
+
+    @patch('weather_app.views.fetch_weather_data')
+    def test_home_view(self, mock_fetch):
+        mock_fetch.return_value = {
+            'temperature': 25.0,
+            'description': 'sunny'
+        }
+
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.city_name)
+        self.assertContains(response, self.weather.temperature)
+        self.assertContains(response, self.weather.description)
+
+    @patch('weather_app.views.fetch_weather_data')
+    def test_add_city(self, mock_fetch):
+        mock_fetch.return_value = {
+            'temperature': 30.0,
+            'description': 'hot'
+        }
 
         response = self.client.post(reverse('home'), {'name': 'New York'})
         self.assertEqual(response.status_code, 302)  # Redirects after POST
